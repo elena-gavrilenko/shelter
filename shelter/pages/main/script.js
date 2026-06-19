e41f63b898d4497c8517e8314348036eebd3521d
Helena <gomel-offis@mail.ru>
2026-06-19 22:34:52 +0300

    feat: implement burger-menu (Fri, Jun 19, 2026 10:34:51 PM)

--- shelter/pages/main/script.js
+++ shelter/pages/main/script.js
@@ -1,1 +1,17 @@
-console.log('110 все критерии соответствуют ТЗ');
+const body = document.querySelector('body');
+const burger = document.querySelector('.header__burger');
+const nav = document.querySelector('.header__menu');
+
+burger.addEventListener('click', () => {
+  burger.classList.toggle('header__burger_active');
+  nav.classList.toggle('header__menu_open');
+  body.classList.toggle('scroll');
+});
+
+function classChange() {
+  burger.classList.remove('header__burger_active');
+  nav.classList.remove('header__menu_open');
+  body.classList.remove('scroll');
+}
+const navItems = document.querySelectorAll('.header__menu-item');
+navItems.forEach((elem) => elem.addEventListener('click', classChange));
